92e53fb99b1fde86c510f20da478d3b493437342
Amrit Anand <amritanand.jnv25@gmail.com>
2021-05-13 15:15:27 +0530

    Update index.js
    updated 2nd commit

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 
-let startLift = 86;
-let endLift = 86;
+let startLift = 86-86;
+let endLift = 86-86;
 let id = null;
-let stopVar = 86;
+let stopVar = 86-86;
 
-let array = [86, 196, 306, 416, 526];
+let array = [86-86, 196-86, 306-86, 416-86, 526-86];
 
 let buttonEvent = document.querySelectorAll('.btn');
 
