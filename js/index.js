e86d2ec432f4088ba91611ec4cbaffe2c3eefbda
Amrit Anand <amritanand.jnv25@gmail.com>
2021-05-13 15:02:07 +0530

    lift simulation
    My first commit

---
+++ js/index.js
@@ -0,0 +1,74 @@
+
+let startLift = 86;
+let endLift = 86;
+let id = null;
+let stopVar = 86;
+
+let array = [86, 196, 306, 416, 526];
+
+let buttonEvent = document.querySelectorAll('.btn');
+
+buttonEvent.forEach( element => {
+    if(element.id != "stop")
+    {
+        document.getElementById(`${element.id}`).addEventListener("click",function(){move(`${element.id}`)});
+    }else{
+        document.getElementById("stop").addEventListener("click", function(){
+            let endStop = stopVar;
+        clearInterval(id);
+        endLift = endStop;
+        });   
+    }
+   
+})
+
+function move(idValue)
+{
+  startLift = endLift;
+  let end = array[idValue - 1];
+  let liftMove = end - startLift;
+  let start = document.getElementById("mylift");
+  animation(startLift, end, liftMove, start);
+  endLift = end;
+}
+
+function animation (start, end, move, start1){
+    let position = start;
+    clearInterval(id);
+    id = setInterval(() => {
+        if (position == end) {
+            clearInterval(id);
+        } else {
+            if (move < 0) {
+                position--;
+            } else {
+                position++;
+            }
+            stopVar = position;
+            start1.style.bottom = position + "px";
+        }
+
+    }, 14);
+}
+
+
+let error = document.getElementById('error');
+let liftsec = document.getElementById('liftsection');
+
+if(window.screen.availWidth === 1500)
+{
+//   console.log(window.screen.availWidth);
+  liftsec.style.display = "flex";
+  error.style.display = "none";
+}
+if(window.screen.availWidth < 1500)
+{
+    liftsec.style.display ="none";
+    error.style.display = "flex";
+    error.innerHTML = "<h1>Please open in screen size of 1500 and above</h1>"
+}
+// else{
+//   liftsec.style.display = "none";
+//   error.style.display = "flex";
+// }
+
